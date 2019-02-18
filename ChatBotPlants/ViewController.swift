5f30b0c3fd6a9ef306938d10cf88231f26506371
Francesca <fharo@belatrixsf.com>
2019-02-18 09:29:36 -0500

    Initial Commit

---
+++ ChatBotPlants/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatBotPlants
+//
+//  Created by Francesca Valeria Haro Dávila on 2/18/19.
+//  Copyright © 2019 Belatrix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
